d1414b7cb24eb692ea076f536f12c0539705dc15
terry_love_c@sina.com <terry_love_c@sina.com>
2015-10-04 23:09:29 +0800

    20151004

--- omret/1/omret/omretuser/models.py
+++ omret/1/omret/omretuser/models.py
@@ -7,5 +7,17 @@ from omret.logret import models as logret_models
 ##------user profile table----------
 class UserProfile(models.Model):
     user = models.OneToOneField(logret_models.User)
-    realname = models.CharField
+    realname = models.CharField(max_length=30)
+    sex = models.BooleanField()
+    birthday = models.DateField(auto_now=False,auto_now_add=False)
+    signature = models.TextField(max_length=120)
+    resume = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+##------user relationship------
+class UserRelationship(models.Model):
+    user =
+    user_follow =
+    user_followers =
