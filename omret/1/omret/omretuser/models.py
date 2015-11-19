50f1907c3ef231359660aa43a97c127666b37fdc
terry_love_c@sina.com <terry_love_c@sina.com>
2015-11-19 23:10:23 +0800

    20151119

--- omret/1/omret/omretuser/models.py
+++ omret/1/omret/omretuser/models.py
@@ -7,7 +7,7 @@ from omret.logreg import models as logreg_models
 ##------user profile table----------
 class UserProfile(models.Model):
     user = models.OneToOneField(logreg_models.User)
-    realname = models.CharField(max_length=30)
+    realname = models.CharField(max_length=50)
     sex = models.CharField(choices=(('F','Female'),('M','Male')),max_length=5)
     birthday = models.DateField(auto_now=False,auto_now_add=False)
     #birthday = models.CharField(max_length=30)
@@ -18,10 +18,10 @@ class UserProfile(models.Model):
         return self.user.name
 
 ##------user relationship------
-class UserRelationship(models.Model):
-    user = models.ForeignKey(logreg_models.User)
-    user_follow = models.IntegerField()
-    user_followers = models.IntegerField()
-
-    def __str__(self):
-        return self.user.name + 'follow: ' + user_follow + 'followers: ' + user_followers
+#class UserRelationship(models.Model):
+#    user = models.ForeignKey(logreg_models.User)
+#    user_follow = models.IntegerField()
+#    user_followers = models.IntegerField()
+#
+#    def __str__(self):
+#        return self.user.name + 'follow: ' + user_follow + 'followers: ' + user_followers
