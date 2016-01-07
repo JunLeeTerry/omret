754ed9dc534777628b048a51480d6c40d40d8702
terry_love_c@sina.com <terry_love_c@sina.com>
2016-01-07 12:57:09 +0800

    modify script

--- commitomret.sh
+++ commitomret.sh
@@ -2,16 +2,17 @@
 
 message=`date +%Y%m%d`
 cd ~/git_JunLeeTerry/omret;
+git checkout develop
 cp -r ~/omret_9_17/omret ~/git_JunLeeTerry/omret/;
 git add .;
 
-echo "Enter the commit message: \c"
-read INPUT
-if [ "$INPUT" = "" ] ; then
-    git commit -m $message;
-else 
-    git commit -m $INPUT;
-fi
+#echo "Enter the commit message: \c"
+#read INPUT
+#if [ "$INPUT" = "" ] ; then
+#    git commit -m $message;
+#else 
+#    git commit -m $INPUT;
+#fi
 
-#git commit -m $message;
-git push;
+git commit -m $message;
+git push origin develop;
