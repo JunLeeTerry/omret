759a8a15f682e2987af935fd2ae5658d93ba4a84
terry_love_c@sina.com <terry_love_c@sina.com>
2016-03-12 21:19:01 +0800

    add newcomments model

--- omret/1/omret/omretnews/models.py
+++ omret/1/omret/omretnews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from omret.logreg import models as logreg_models
 
 # Create your models here.
 class OmretNews(models.Model):
@@ -17,4 +18,10 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
-
+class NewComments(models.Model):
+    article_id = models.ForeignKey('OmretNews')
+    #comments under the commnent
+    comment_id = models.ForeignKey('NewComments')
+    comment_content = models.CharField(max_length=200)
+    comment_time = models.DateTimeField(auto_now_add=True)
+    comment_user = models.OneToOneField(logreg_models.User)
