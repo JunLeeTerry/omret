3adc9fb77b0b02be402f22b11b23def45a7550ef
jinan1006 <853519127@qq.com>
2016-04-19 22:23:26 +0800

    limitWord

---
+++ omret/1/omret/static/js/util.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2016/4/16.
+ */
+function wordTip(dom, maxSize) {
+    var tip;
+    if (dom.val().length > maxSize) {
+        dom.value = dom.value.substring(0, maxSize);
+    }
+    var remainSize = maxSize - dom.val().length;
+    tip = $("<div class='tip'>(" + remainSize + "/" + maxSize + ")</div>");
+    $(tip).after(dom);
+    //$('<div>123</div>').after(dom);
+
+    var left = dom.offset().left;
+    var top = dom.offset().top;
+    var width = dom.width();
+    var height = dom.height();
+    $(tip).css({
+        'left': left + width - 60,
+        'top': top + height - 8
+    });
+    $(tip).show();
+
+}
