b82fb9bada2f3a6d9aa8346603505211365b58f3
jinan1006 <853519127@qq.com>
2016-04-19 23:26:12 +0800

    fix bug of limit words

--- omret/1/omret/static/js/counter.js
+++ omret/1/omret/static/js/counter.js
@@ -3,22 +3,20 @@
  */
 function wordTip(dom, maxSize) {
     var tip;
+    $('.tip').remove();
     if (dom.val().length > maxSize) {
         dom.value = dom.value.substring(0, maxSize);
     }
     var remainSize = maxSize - dom.val().length;
     tip = "<div class='tip'>(" + remainSize + "/" + maxSize + ")</div>";
     $(dom).after(tip);
-    //$('<div>123</div>').after(dom);
 
-    var left = dom.offset().left;
+    //var left = dom.offset().left;
     var top = dom.offset().top;
     var width = dom.width();
-    var height = dom.height();
-    $(tip).css({
-        'left': left + width - 60,
-        'top': top + height - 8
+    //var height = dom.height();
+    $('.tip').css({
+        'left': width - 60,
+        'top': top  - 8
     });
-    $(tip).show();
-
 }
