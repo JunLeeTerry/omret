893112189d94b8302725cd7af93bfdac0df6c45f
terry_love_c@sina.com <terry_love_c@sina.com>
2016-05-05 00:55:49 +0800

    use jcrop

--- omret/1/omret/static/js/omretuser/headset.js
+++ omret/1/omret/static/js/omretuser/headset.js
@@ -2,24 +2,7 @@
  * Created by terry on 5/3/16.
  */
 $(function () {
-    var $dataX = $('#dataX');
-    var $dataY = $('#dataY');
-    var $dataHeight = $('#dataHeight');
-    var $dataWidth = $('#dataWidth');
-    var $dataRotate = $('#dataRotate');
-    var $dataScaleX = $('#dataScaleX');
-    var $dataScaleY = $('#dataScaleY');
-    var options = {
-        aspectRatio: 1 / 1,
-        preview: '.img-preview',
-        crop: function (e) {
-            $dataX.val(Math.round(e.x));
-            $dataY.val(Math.round(e.y));
-            $dataHeight.val(Math.round(e.height));
-            $dataWidth.val(Math.round(e.width));
-            $dataRotate.val(e.rotate);
-            $dataScaleX.val(e.scaleX);
-            $dataScaleY.val(e.scaleY);
-        }
-    };
+    //var image = $('canvaspart').closest('img');
+    //image.Jcrop();
+    $('#jcropimg').Jcrop();
 });
